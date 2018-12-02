848d48c5c90833a9a7afedece47ea1c92d470624
Ian <iansedano@gmail.com>
2018-12-02 21:40:42 +0100

    developed dictionary for md5 and filepath, and export to json functionaliry

---
+++ manipulating_dict.py
@@ -0,0 +1,8 @@
+import json
+
+import os
+
+my_dict = json.load(open("txt_dict"))
+
+print(my_dict['3fe4f137f276a5f0a2ce09b0bb6fa0bd'])
+
--- setlist_initial.py
+++ setlist_initial.py
@@ -10,7 +10,7 @@ os.chdir(full_path_to_folder)
 
 #set list names
 set_lists = ('open_mic', 'jam_session', 'solo_acoustic', 'piano', 'learning',
-    'looper', 'kids')
+    'looper', 'kids', 'pop')
 
 #making list of paths
 list_of_full_paths = []
@@ -32,7 +32,7 @@ def print_set_list(set_list):
         if re.search(regex, path):
             print(path)
 
-print_set_list('learning')
+#print_set_list('learning')
 
 def open_set_list(set_list):
     regex = "\[.*" + set_list + ".*\]"
@@ -48,3 +48,53 @@ def remove_all_tags():
     for path in list_of_txt_paths:
         print(re.sub('\[.*\]', '', path))
 
+
+#generating md5
+import hashlib
+
+def md5_list(list_of_paths):
+    md5s = []
+    for path in list_of_paths:
+        md5s.append(hashlib.md5(open(path, 'rb').read()).hexdigest())
+    return(md5s)
+
+md5_txts = md5_list(list_of_txt_paths)
+
+#making dictionary with md5s as keys
+keys = md5_txts
+values = list_of_txt_paths
+txt_dict = dict(zip(keys,values))
+print(txt_dict)
+
+
+#changing working directory to script
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
+#writing dict to file
+import json
+
+json.dump(txt_dict, open("txt_dict",'w'))
+
+
+# script to get creation date for file to check if same file...
+import platform
+
+def creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
